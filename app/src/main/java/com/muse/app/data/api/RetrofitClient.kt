3beba8278f8412d05fbbba950aedaaa2dbaf7ee4
sd-community1 <xvvv99634@gmail.com>
2026-06-23 14:13:48 +0300

    Add RetrofitClient for YouTube API integration

---
+++ app/src/main/java/com/muse/app/data/api/RetrofitClient.kt
@@ -0,0 +1,26 @@
+package com.muse.app.data.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+
+    private const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(15, TimeUnit.SECONDS)
+        .readTimeout(15, TimeUnit.SECONDS)
+        .build()
+
+    val youtubeApi: YoutubeApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(YoutubeApiService::class.java)
+    }
+}
